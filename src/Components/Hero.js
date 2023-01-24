cbcc533619f3752dd9add1455863d6152c3b62db
zgronska <zgronska@gmail.com>
2023-01-24 15:47:39 +0100

    Customizing the hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,10 @@
-import { Grid, Typography, Button } from '@mui/material'
+import { Grid, Typography, IconButton, Slide } from '@mui/material'
+import { TransitionGroup } from 'react-transition-group'
 import HeaderBg from '../Assets/bg-1.jpg'
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown'
+import 'animate.css'
+import React, { useState, useEffect } from 'react'
+import useTimeout from 'useTimeout'
 
 const Hero = () => {
   //Smooth scrolling on CTA
@@ -11,9 +15,84 @@ const Hero = () => {
     }
   }
 
+  //Button animation stop
+  function ScrollBtn() {
+    const [hovered, setHovered] = useState(false)
+    const toggleHover = () => setHovered(!hovered)
+    return (
+      <IconButton
+        className={hovered ? '' : 'animate__animated animate__bounce animate__infinite'}
+        onMouseEnter={toggleHover}
+        onMouseLeave={toggleHover}
+        variant="outlined"
+        color="secondary"
+        onClick={handleClickScroll}
+      >
+        <KeyboardDoubleArrowDownIcon />
+      </IconButton>
+    )
+  }
+
+  //Slide animation
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      console.log('This will be called after 2 seconds')
+    }, 2000)
+
+    return () => clearTimeout(timeout)
+  }, [])
+
+  function TextSlide() {
+    const [hasTimeElapsed, setHasTimeElapsed] = React.useState(false)
+
+    const content = (
+      <TransitionGroup>
+        <Slide direction="right" timeout={500}>
+          <Typography
+            variant="h1"
+            sx={{
+              textShadow: `${textShadow}`,
+            }}
+          >
+            {title}
+          </Typography>
+        </Slide>
+        <Slide direction="right" timeout={1000}>
+          <Typography
+            variant="h2"
+            color="secondary"
+            sx={{
+              textShadow: `${textShadow}`,
+            }}
+          >
+            {subtitle}
+          </Typography>
+        </Slide>
+      </TransitionGroup>
+    )
+
+    useTimeout(() => {
+      setHasTimeElapsed(true)
+    }, 1000)
+    return (
+      <Grid
+        item
+        container
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        sx={{
+          flex: 1,
+        }}
+      >
+        {hasTimeElapsed ? content : ''}
+      </Grid>
+    )
+  }
+
+  //Content and styling
   const title = "Hi, I'm Zuza"
   const subtitle = 'Front-End Developer'
-  const btnText = 'See my projects'
 
   const textShadow = '.08em .08em 0 #3E60B8'
 
@@ -23,44 +102,20 @@ const Hero = () => {
       alignItems="center"
       justifyContent="center"
       direction="column"
+      textAlign="center"
       sx={{
         height: '100vh',
         background: `linear-gradient(180deg, rgba(23, 2, 80, .1) 0%, rgba(23, 2, 80, .2) 50%, rgba(23, 2, 80, 1) 100%) scroll, url(${HeaderBg}) no-repeat fixed center`,
         backgroundSize: 'cover',
+        p: 6,
       }}
       component={'header'}
       className="hero-section"
     >
+      <TextSlide />
+
       <Grid item>
-        <Typography
-          variant="h1"
-          sx={{
-            textShadow: `${textShadow}`,
-          }}
-        >
-          {title}
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Typography
-          variant="h2"
-          color="secondary"
-          sx={{
-            textShadow: `${textShadow}`,
-          }}
-        >
-          {subtitle}
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Button
-          variant="contained"
-          color="secondary"
-          startIcon={<KeyboardDoubleArrowDownIcon />}
-          onClick={handleClickScroll}
-        >
-          {btnText}
-        </Button>
+        <ScrollBtn />
       </Grid>
     </Grid>
   )
