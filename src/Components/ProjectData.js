29e8a82be260bbcae1cf570b3cf59af57c05ff58
zgronska <zgronska@gmail.com>
2024-10-14 18:17:11 +0200

    Removed old broken projects

--- src/Components/ProjectData.js
+++ src/Components/ProjectData.js
@@ -10,15 +10,6 @@ export const projectdata = [
   },
   {
     id: 2,
-    title: 'Inspirational Quotes Generator',
-    description: 'Simple landing page generating inspirational quotes from the Quotable API.',
-    tags: ['HTML', 'CSS', 'JavaScript', 'API'],
-    image: 'quotes.png',
-    code: 'https://github.com/zgronska/inspirational-quotes',
-    live: 'https://zgronska.github.io/inspirational-quotes/',
-  },
-  {
-    id: 3,
     title: 'Pet Age Converter',
     description: `This app provides a simple and easy way to convert your pet's age into human years.`,
     tags: ['HTML', 'CSS', 'JavaScript'],
@@ -27,30 +18,12 @@ export const projectdata = [
     live: 'https://quiet-clafoutis-975c0a.netlify.app/',
   },
   {
-    id: 4,
+    id: 3,
     title: 'Travel Journal',
     description: 'Course project documenting my first steps with React.',
     tags: ['React', 'CSS', 'JavaScript'],
     image: 'traveljournal.png',
     code: 'https://github.com/zgronska/travel-journal',
     live: 'https://darling-duckanoo-f2e138.netlify.app/',
-  },
-  {
-    id: 5,
-    title: 'Password Generator',
-    description: 'JavaScript practice, with a funky dark mode toggle.',
-    tags: ['HTML', 'CSS', 'JavaScript'],
-    image: 'passgen.png',
-    code: 'https://github.com/zgronska/password-generator',
-    live: 'https://zgronska.github.io/password-generator/',
-  },
-  {
-    id: 6,
-    title: 'CoolBeans',
-    description: 'Landing page for a fictional cafe with various features and responsive design.',
-    tags: ['HTML', 'CSS', 'JavaScript'],
-    image: 'coolbeans.png',
-    code: 'https://github.com/zgronska/top-landing-page',
-    live: 'https://zgronska.github.io/top-landing-page/',
-  },
+  }
 ]
